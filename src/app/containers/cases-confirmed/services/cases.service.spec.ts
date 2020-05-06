441496a8b960ab25f56a5e08e06ba9a3931df24b
Alver Grisales <alexander.grisales@almundo.com>
2020-05-06 01:05:21 -0500

    feat: add pipe for searching cities o states

--- src/app/containers/cases-confirmed/services/cases.service.spec.ts
+++ src/app/containers/cases-confirmed/services/cases.service.spec.ts
@@ -4,18 +4,26 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 // Services
 import { CasesService } from './cases.service';
+import { getCases, getLoading } from '../store/selectors/cases-confirmed.selectors';
 
 describe('CasesService', () => {
   let service: CasesService;
   let store: MockStore;
 
   const initialState = { loading: false, data: {} };
+  const data = { total: 10, casesByCity: [{ name: 'Cali', total: 1 }], casesByState: [{ name: 'Cali', total: 1 }] };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        provideMockStore({ initialState }),
-      ]
+        provideMockStore({
+          initialState,
+          selectors: [
+            { selector: getCases, value: data },
+            { selector: getLoading, value: data },
+          ],
+        }),
+      ],
     });
     service = TestBed.inject(CasesService);
     store = TestBed.inject(MockStore);
@@ -31,15 +39,17 @@ describe('CasesService', () => {
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
-  it('should get the cases of the store', () => {
-    const spy = jest.spyOn(store, 'select');
-    service.getCases$();
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('should get the cases of the store', (done) => {
+    service.getCases$().subscribe((response) => {
+      expect(response).toEqual(data);
+      done();
+    });
   });
 
-  it('should get the loading of the store', () => {
-    const spy = jest.spyOn(store, 'select');
-    service.getLoading$();
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('should get the loading of the store', (done) => {
+    service.getLoading$().subscribe((response) => {
+      expect(response).toBeTruthy();
+      done();
+    });
   });
 });
--- src/app/pipes/search-text/search-text.pipe.spec.ts
+++ src/app/pipes/search-text/search-text.pipe.spec.ts
@@ -1,8 +1,32 @@
 import { SearchTextPipe } from './search-text.pipe';
 
 describe('SearchTextPipe', () => {
+  let pipe: SearchTextPipe;
+  const data = [
+    { name: 'alver', total: 1 },
+    { name: 'angie', total: 1 },
+  ];
+
+  beforeEach(() => {
+    pipe = new SearchTextPipe();
+  });
   it('create an instance', () => {
-    const pipe = new SearchTextPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return an array of one position', () => {
+    expect(pipe.transform(data, { key: 'name', value: 'alver' })).toHaveLength(1);
+  });
+
+  it('should return the same array when not send arguments', () => {
+    expect(pipe.transform(data, {} as any)).toEqual(data);
+  });
+
+  it('should return the same array when the length of the value is less than three', () => {
+    expect(pipe.transform(data, { key: 'name', value: 'al' })).toEqual(data);
+  });
+
+  it('should return an array empty when send paramaters empty', () => {
+    expect(pipe.transform(null, null)).toEqual([]);
   });
 });
